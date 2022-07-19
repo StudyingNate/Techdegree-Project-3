81e5703876b1111db78efb34330e35adede87646
Nate <studyingnate@gmail.com>
2022-07-18 22:52:50 -0700

    T-Shirt Info
    Variables and Event Listener that will enable the "Color" field when a "Design" is selected while filtering the color options based on design selected.

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -5,8 +5,8 @@ document.getElementById("name").focus();
 const jobRole =  document.getElementById("title");
 const otherJob = document.getElementById("other-job-role").style.display = "none";
 
-//Toggle for between showing text input for "Other"
-// https://www.w3schools.com/ used for "block" reference 
+//Toggle for between showing text input for "Other".
+// https://www.w3schools.com/ used for "block" reference. 
 jobRole.addEventListener("change", (e) => {
     if(e.target.value === "other") {
         document.getElementById("other-job-role").style.display = "block";
@@ -15,3 +15,26 @@ jobRole.addEventListener("change", (e) => {
     };
 });
 
+// Variables for selecting the "Design" & "Color" elements.
+// Color element is disabled by default on page load.
+const shirtDesign = document.getElementById("design");
+const shirtColor = document.getElementById("color")
+    document.getElementById("color").disabled = true;
+const shirtTheme = shirtColor.children;
+//console.log(shirtTheme);
+
+/*Event listener that will enable the "Color:" field if "Design" selected from the dropdown menu 
+while hiding those that do not fall under that theme.*/
+shirtDesign.addEventListener("change", (e) => {
+     document.getElementById("color").disabled = false;
+     for (let i = 0; i < shirtTheme.length; i++) {
+         if (e.target.value === shirtTheme[i].getAttribute("data-theme")){
+            shirtTheme[i].hidden = false;
+            shirtTheme[i].setAttribute("selected", true);
+         } else {
+            shirtTheme[i].hidden = true;
+            shirtTheme[i].removeAttribute("selected");
+         };
+        
+     };
+});
