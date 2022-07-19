48d70302f379e89da6625e287bdb351ce933845c
Nate <studyingnate@gmail.com>
2022-07-18 20:25:09 -0700

    "Job Role" section
    Created the two variables that..
    1. Selects the element for the Job Role
    2. Hides "Other" role be default.
    
    Created an event listener that will listen for "change" on the dropdown menu if..
    1. Change equals "other" then display text input field.
    2. Change does not equal "other" then stay hidden.

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,2 +1,16 @@
 //Element selector to auto-select name field on page load.
 document.getElementById("name").focus();
+
+//Variables to reference job role and to hide the text input for "Other" job role by default. 
+let jobRole =  document.getElementById("title");
+let otherJob = document.getElementById("other-job-role").style.display = "none";
+
+jobRole.addEventListener("change", (e) => {
+    if(e.target.value === "other") {
+        document.getElementById("other-job-role").style.display = "block";
+    } else {
+        document.getElementById("other-job-role").style.display = "none"
+    }
+    
+
+});
