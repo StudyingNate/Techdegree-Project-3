9ab4ea3e092054af53c6806f9821503450d4a0ba
Nate <studyingnate@gmail.com>
2022-07-18 20:34:33 -0700

    Update for last Description
    Code changes for the last update description.

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -2,15 +2,16 @@
 document.getElementById("name").focus();
 
 //Variables to reference job role and to hide the text input for "Other" job role by default. 
-let jobRole =  document.getElementById("title");
-let otherJob = document.getElementById("other-job-role").style.display = "none";
+const jobRole =  document.getElementById("title");
+const otherJob = document.getElementById("other-job-role").style.display = "none";
 
+//Toggle for between showing text input for "Other"
+// https://www.w3schools.com/ used for "block" reference 
 jobRole.addEventListener("change", (e) => {
     if(e.target.value === "other") {
         document.getElementById("other-job-role").style.display = "block";
     } else {
-        document.getElementById("other-job-role").style.display = "none"
-    }
-    
-
+        document.getElementById("other-job-role").style.display = "none";
+    };
 });
+
