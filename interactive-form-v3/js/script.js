fb87fe7cddffd962c69f88536ec7a91165b644a0
Nate <studyingnate@gmail.com>
2022-07-24 23:53:01 -0700

    "Register for Activities"
    Aimed for "Exceeds Expectation" when working on this portion.
    
    1. Variables created to select activities, text input for cost, store cost total, and array that selects items based on their input type.
    
    1. Event listener will check if item is check, and add/remove to cost based on checkbox selection.
    
    2. For loop will determine whether if the item checked off conflicts with any other activities and will disable that item.
    
    3. Else portion of the for loop will remove the disabled activity status if the item is not checked off.

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,9 +1,10 @@
-//Element selector to auto-select name field on page load.
-document.getElementById("name").focus();
+//Variable to auto-select name field on page load.
+const nameField = document.getElementById("name").focus();
+
 
 //Variables to reference job role and to hide the text input for "Other" job role by default. 
-const jobRole =  document.getElementById("title");
-const otherJob = document.getElementById("other-job-role").style.display = "none";
+let jobRole =  document.getElementById("title");
+let otherJob = document.getElementById("other-job-role").style.display = "none";
 
 //Toggle for between showing text input for "Other".
 // https://www.w3schools.com/ used for "block" reference. 
@@ -12,19 +13,20 @@ jobRole.addEventListener("change", (e) => {
         document.getElementById("other-job-role").style.display = "block";
     } else {
         document.getElementById("other-job-role").style.display = "none";
-    };
+    }
 });
 
 // Variables for selecting the "Design" & "Color" elements.
 // Color element is disabled by default on page load.
 const shirtDesign = document.getElementById("design");
 const shirtColor = document.getElementById("color")
-    document.getElementById("color").disabled = true;
+    shirtColor.disabled = true;
 const shirtTheme = shirtColor.children;
-//console.log(shirtTheme);
 
-/*Event listener that will enable the "Color:" field if "Design" selected from the dropdown menu 
-while hiding those that do not fall under that theme.*/
+/*
+    Event listener that will enable the "Color:" field if "Design" selected from the dropdown 
+    menu while hiding those that do not fall under that theme.
+*/
 shirtDesign.addEventListener("change", (e) => {
      document.getElementById("color").disabled = false;
      for (let i = 0; i < shirtTheme.length; i++) {
@@ -34,7 +36,49 @@ shirtDesign.addEventListener("change", (e) => {
          } else {
             shirtTheme[i].hidden = true;
             shirtTheme[i].removeAttribute("selected");
-         };
-        
-     };
+         }
+     }
 });
+
+
+/*  
+    Create variables that select the <fieldset> and <p> elements, 
+    Create variable for starting a total cost amount to 0.
+    Create variable that will hold the array of checkbox.
+*/
+const registerForActivity = document.getElementById("activities");
+const totalCost = document.getElementById("activities-cost");
+let addedCost = 0;
+const allEvents = document.querySelectorAll("#activities-box input");
+
+/*Event listner for change on registerForActivity..
+    1.  Checks if target is selected and adds the target cost 
+        to the totalCost, else if not checked removes the cost from totalCost.
+    2.  For loop that will compare date/time for the selected target with array list and disables
+        the selected array position if values are true.
+    3.  If activities are not selected but share the same date/time, removes any activity disabled.
+*/
+registerForActivity.addEventListener("change", (e) => {
+    let eventCost = parseInt(e.target.getAttribute("data-cost"));
+    if (e.target.checked) {
+        addedCost += eventCost;
+    }
+    else {
+        addedCost -= eventCost;    
+    }
+
+    for(let i = 0; i < allEvents.length; i++) {
+        if  (e.target.getAttribute("data-day-and-time") === allEvents[i].getAttribute("data-day-and-time") 
+        && e.target.checked === true) {
+        allEvents[i].disabled = true;
+        e.target.disabled = false;
+        } 
+        else if (e.target.getAttribute("data-day-and-time") === allEvents[i].getAttribute("data-day-and-time") 
+        && e.target.checked === false) {
+        allEvents[i].disabled = false;
+        }
+    }
+    totalCost.innerHTML = `Total: ${addedCost}`;
+});
+
+
