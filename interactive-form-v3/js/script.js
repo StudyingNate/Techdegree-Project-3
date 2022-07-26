10de6a6ac33f0a4e4086a398255df97cac7da615
Nate <studyingnate@gmail.com>
2022-07-25 23:25:38 -0700

    Validation Attempt
    Top to Bottom
    1. Variables that select the input fields of information for this program to successfully submit.
    
    2. Attempted to create a event listener that will accept multiple NOT operators in the event a user does not file input correctly.
    
    3. Did a few refactoring, pulled all the inputs out of the event listener to their own functions and when called upon will return a true or false.
    
    4. Created a helper function that will display set/remove if a field is validated or not.

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,26 +1,27 @@
 //Variable to auto-select name field on page load.
-const nameFocus = document.getElementById("name").focus();
+let nameField = document.getElementById("name");
+nameField.focus();
 
-
-//Variables to reference job role and to hide the text input for "Other" job role by default. 
-let jobRole =  document.getElementById("title");
-let otherJob = document.getElementById("other-job-role").style.display = "none";
+//Variables to reference job role and to hide the text input for "Other" job role by default.
+let jobRole = document.getElementById("title");
+let otherJob = (document.getElementById("other-job-role").style.display =
+  "none");
 
 //Toggle for between showing text input for "Other".
-// https://www.w3schools.com/ used for "block" reference. 
+// https://www.w3schools.com/ used for "block" reference.
 jobRole.addEventListener("change", (e) => {
-    if(e.target.value === "other") {
-        document.getElementById("other-job-role").style.display = "block";
-    } else {
-        document.getElementById("other-job-role").style.display = "none";
-    }
+  if (e.target.value === "other") {
+    document.getElementById("other-job-role").style.display = "block";
+  } else {
+    document.getElementById("other-job-role").style.display = "none";
+  }
 });
 
 // Variables for selecting the "Design" & "Color" elements.
 // Color element is disabled by default on page load.
 const shirtDesign = document.getElementById("design");
-const shirtColor = document.getElementById("color")
-    shirtColor.disabled = true;
+const shirtColor = document.getElementById("color");
+shirtColor.disabled = true;
 const shirtTheme = shirtColor.children;
 
 /*
@@ -28,18 +29,17 @@ const shirtTheme = shirtColor.children;
     menu while hiding those that do not fall under that theme.
 */
 shirtDesign.addEventListener("change", (e) => {
-     document.getElementById("color").disabled = false;
-     for (let i = 0; i < shirtTheme.length; i++) {
-         if (e.target.value === shirtTheme[i].getAttribute("data-theme")){
-            shirtTheme[i].hidden = false;
-            shirtTheme[i].setAttribute("selected", true);
-         } else {
-            shirtTheme[i].hidden = true;
-            shirtTheme[i].removeAttribute("selected");
-         }
-     }
+  document.getElementById("color").disabled = false;
+  for (let i = 0; i < shirtTheme.length; i++) {
+    if (e.target.value === shirtTheme[i].getAttribute("data-theme")) {
+      shirtTheme[i].hidden = false;
+      shirtTheme[i].setAttribute("selected", true);
+    } else {
+      shirtTheme[i].hidden = true;
+      shirtTheme[i].removeAttribute("selected");
+    }
+  }
 });
-
 
 /*  
     Created variables that select the <fieldset> and <p> elements, 
@@ -56,29 +56,34 @@ const allEvents = document.querySelectorAll("#activities-box input");
         to the totalCost, else if not checked removes the cost from totalCost.
     2.  For loop that will compare date/time for the selected target with array list and disables
         the selected array position if values are true.
-    3.  If activities are not selected but share the same date/time, removes any activity disabled.
+    3.  If activities are not selected but share the same date/time, removes disabled activity status.
 */
 registerForActivity.addEventListener("change", (e) => {
-    let eventCost = parseInt(e.target.getAttribute("data-cost"));
-    if (e.target.checked) {
-        addedCost += eventCost;
-    } else {
-        addedCost -= eventCost;    
-    }
+  let eventCost = parseInt(e.target.getAttribute("data-cost"));
+  if (e.target.checked) {
+    addedCost += eventCost;
+  } else {
+    addedCost -= eventCost;
+  }
 
-    for(let i = 0; i < allEvents.length; i++) {
-        if  (e.target.getAttribute("data-day-and-time") === allEvents[i].getAttribute("data-day-and-time") 
-            && e.target.checked === true) {
-            allEvents[i].disabled = true;
-            e.target.disabled = false;
-        } else if (e.target.getAttribute("data-day-and-time") === allEvents[i].getAttribute("data-day-and-time") 
-            && e.target.checked === false) {
-            allEvents[i].disabled = false;
-        }
+  for (let i = 0; i < allEvents.length; i++) {
+    if (
+      e.target.getAttribute("data-day-and-time") ===
+        allEvents[i].getAttribute("data-day-and-time") &&
+      e.target.checked === true
+    ) {
+      allEvents[i].disabled = true;
+      e.target.disabled = false;
+    } else if (
+      e.target.getAttribute("data-day-and-time") ===
+        allEvents[i].getAttribute("data-day-and-time") &&
+      e.target.checked === false
+    ) {
+      allEvents[i].disabled = false;
     }
-    totalCost.innerHTML = `Total: ${addedCost}`;
+  }
+  totalCost.innerHTML = `Total: ${addedCost}`;
 });
-
 
 /*
     Create variables that select Paypal,Bitcoin, Credit Card and User payment type
@@ -99,19 +104,104 @@ payPal.style.display = "none";
     payment method if event target equal to selected option.
 */
 userPayment.addEventListener("change", (e) => {
-    if (e.target.value === "bitcoin") {
-        document.getElementById("bitcoin").style.display = "block";
-        creditCard.style.display = "none";
-        payPal.style.display = "none";
-     } else if (e.target.value === "paypal") {
-        document.getElementById("paypal").style.display = "block";
-        creditCard.style.display = "none";
-        bitCoin.style.display = "none";
-     } else {
-        document.getElementById("credit-card").style.display = "block";
-        bitCoin.style.display = "none";
-        payPal.style.display = "none";
-     } 
-    });
+  if (e.target.value === "bitcoin") {
+    document.getElementById("bitcoin").style.display = "block";
+    creditCard.style.display = "none";
+    payPal.style.display = "none";
+  } else if (e.target.value === "paypal") {
+    document.getElementById("paypal").style.display = "block";
+    creditCard.style.display = "none";
+    bitCoin.style.display = "none";
+  } else {
+    document.getElementById("credit-card").style.display = "block";
+    bitCoin.style.display = "none";
+    payPal.style.display = "none";
+  }
+});
 
+/* 
+    Create variables to select items below..
+    Name, Email, Register for Activities, Card #, Zip, CVV and form element 
+    Note: Name and Register for Activities variables already created.
+*/
+let emailField = document.getElementById("email");
+let cardField = document.getElementById("cc-num");
+let zipField = document.getElementById("zip");
+let cvvField = document.getElementById("cvv");
+const formField = document.querySelector("form");
 
+formField.addEventListener("submit", (e) => {
+    if (!nameValidated ||  !emailValidated || 
+        !cardValidated || !zipValidated ||
+        !cvvValidated) {
+        e.preventDefault();
+        }
+         
+});
+
+const nameValidated = () => {
+  let nameSubmit = nameField.value;
+  validName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/.test(nameSubmit);
+  if (validName) {
+    validate(nameSubmit);
+  } else {
+    invalid(nameSubmit);
+  }
+};
+
+const emailValidated = () => {
+  let emailSubmit = emailField.value;
+  validEmail =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      emailSubmit
+    );
+  if (validEmail) {
+    validate(emailSubmit);
+  } else {
+    invalid(emailSubmit);
+  }
+  return nameValidated;
+};
+
+const cardValidated = () => {
+  let cardSubmit = cardField.value;
+  validCard = /^\d{13,16}$/.test(cardSubmit);
+  if (validCard) {
+    validate(cardSubmit);
+  } else {
+    invalid(cardSubmit);
+  }
+  return cardValidated;
+};
+const zipValidated = () => {
+  let zipSubmit = zipField.value;
+  validZip = /^[0-9]{5}$/.test(zipSubmit);
+  if (validZip) {
+    validate(zipSubmit);
+  } else {
+    invalid(zipSubmit);
+  }
+  return zipValidated;
+};
+
+const cvvValidated = () => {
+  let cvvSubmit = cvvField.value;
+  validCvv = /^[0-9]{3}$/.test(cvvSubmit);
+  if (validCvv) {
+    validate(cvvSubmit);
+  } else {
+    invalid(cvvSubmit);
+  }
+  return cvvValidated;
+};
+function validate(element) {
+  element.parentElement.classList.add("valid");
+  element.parentElement.classList.remove("not-valid");
+  element.parentElement.lastElementChild.style.display = "none";
+}
+
+function invalid(element) {
+  element.parentElement.classList.add("not-valid");
+  element.parentElement.classList.remove("valid");
+  element.parentElement.lastElementChild.style.display = "block";
+}
